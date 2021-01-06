5e729e4353661156e04587198399b3e5f11d0b45
remichartier <remipr.chartier@gmail.com>
2021-01-06 07:59:06 +0000

    first commit, original files

---
+++ bashrc.sh
@@ -0,0 +1,6 @@
+git config --global user.email "remipr.chartier@gmail.com"
+git config --global user.name "remichartier"
+# git remote set-url origin "https://github.com/remichartier/015_selfDrivingCarND_ExtendedKalmanFilterProject.git"
+git remote set-url origin "git@github.com:remichartier/017_selfDrivingCarND_LocalizerKidnappedVehicleProject.git"
+eval "$(ssh-agent -s)"
+ssh-add .ssh/20201224_1219_github_ed25519
